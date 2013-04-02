4ed8006cf0c48b7b554a2a8d55b0f3a3c941d399
teejay-liverpool <timjones1024@gmail.com>
2013-04-02 19:11:23 +0100

    xxx

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
-end
+	def full_title( page_title )
+		baseTitle = 'This is the base title'
+		if page_title.empty?
+			baseTitle
+		else
+			"#{baseTitle} | #{page_title}"
+		end
+	end
+end	
