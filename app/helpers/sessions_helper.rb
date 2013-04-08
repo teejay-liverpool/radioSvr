a0d137a7c831da12473b6ca07781e14936164b60
teejay-liverpool <timjones1024@gmail.com>
2013-04-08 20:44:28 +0100

    Save sign in

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,14 @@
+module SessionsHelper
+	def sign_in(user)
+		cookies.permanent[:remember_token] = user.remember_token
+		self.current_user = user
+	end
+
+	def cuurent_user
+		@current_user ||= User.find_by_remember_token(cookies[:remember_token])
+	end
+
+	def signed_in?
+		!current_user.nil?
+	end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -21,4 +22,11 @@ class User < ActiveRecord::Base
   validates(:password_confirmation, presence: true)
 
   before_save { | user | user.email = email.downcase }
+  before_save :create_remember_token
+
+
+  private 
+  	def create_remember_token
+  		self.remember_token = SecureRandom.urlsafe_base64
+  	end
 end
