4ea1ca3a8ebc268a3bde2d3bffab2beb3423f17c
teejay-liverpool <timjones1024@gmail.com>
2013-04-13 21:01:07 +0100

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,16 @@ DemoApp::Application.routes.draw do
   root to: 'static_pages#home'
 
   match '/signup' , to: 'users#new'
+
   match '/signin' , to: 'sessions#new'
+
   match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help' , to: 'static_pages#help'
 
   match '/about' , to: 'static_pages#about'
 
+  match '/contact_us' , to: 'static_pages#contact_us'
 
   
   # The priority is based upon order of creation:
